765cc8bd7d3d3b8aab51644e41352a3031069408
은선 남궁 <nammugmuhg@gmail.com>
2021-08-17 00:16:50 +0900

    make environment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      프로젝트 시작
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+export default function Routes(params) {
+    return (
+        <BrowserRouter>
+            <>
+                <Switch>
+                  <Route></Route>
+                </Switch>
+            </>
+        </BrowserRouter>
+    )
+}
