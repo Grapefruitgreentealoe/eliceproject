a9227fbeb425ddacad441f2450d29ddb726c9a48
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-08-25 02:29:16 +0900

    한개만 눌러도 넘어감

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { QRadioInput } from './components/RadioInput';
 import { useAsync } from 'react-async';
 
-
 async function getQuestion() {
   const response = await axios.get(
     'https://inspct.career.go.kr/openapi/test/questions?apikey=4848423aeee0be0d33a5f674f4383583&q=6',
@@ -11,8 +10,7 @@ async function getQuestion() {
 
   return response.data.RESULT;
 }
-function Questions({onClick}) {
-
+function Questions({ onClick }) {
   const {
     loading,
     data: questions,
@@ -25,20 +23,28 @@ function Questions({onClick}) {
   const chkarr = new Array(len).fill(0);
 
   const [chkstate, setChkstate] = useState(chkarr);
+  const [allchked, setAllChked] = useState(false);
 
   const handleChange = (e, question) => {
     setChkstate((state) => {
       const newArr = [...state];
       newArr[question.qitemNo - 1] = Number(e.target.value);
       console.log(newArr);
+      if (Math.min(chkarr.slice(page * 5, page * 5 + 5)) !== 0) {
+        setAllChked(true);
+      }
       return newArr;
     });
+  };
+
+  const handleNextButton = () => {
+    setPage(page + 1);
+    setAllChked(false);
   };
 
   // eslint-disable-next-line
   const [page, setPage] = useState(0);
   // eslint-disable-next-line
-  
 
   useEffect(() => {
     run();
@@ -51,42 +57,41 @@ function Questions({onClick}) {
 
   return (
     <>
-  {questions.map((question, index) => (
-    <ul>
-      {index >= page * 5 && index <= page * 5 + 4 ? (
-        <li key={question.qitemNo}>
-          {question.question}
-          <p>
-            <QRadioInput
-              name="question"
-              values={[
-                {
-                  label: question.answer01,
-                  value: 1,
-                },
-                {
-                  label: question.answer02,
-                  value: 2,
-                },
-              ]}
-              onClick={(e) => handleChange(e, question)}
-              chked={chkstate[index]}
-            />
-          </p>
-        </li>
-      ) : null}
-    </ul>
-  ))}
-    {page != questions.length / 4 - 2 ? (
-        <button onClick={() => setPage(page + 1)}>다음</button>
+      {questions.map((question, index) => (
+        <ul>
+          {index >= page * 5 && index <= page * 5 + 4 ? (
+            <li key={question.qitemNo}>
+              {question.question}
+              <p>
+                <QRadioInput
+                  name="question"
+                  values={[
+                    {
+                      label: question.answer01,
+                      value: 1,
+                    },
+                    {
+                      label: question.answer02,
+                      value: 2,
+                    },
+                  ]}
+                  onClick={(e) => handleChange(e, question)}
+                  chked={chkstate[index]}
+                />
+              </p>
+            </li>
+          ) : null}
+        </ul>
+      ))}
+      {page != questions.length / 4 - 2 ? (
+        <button onClick={allchked ? handleNextButton() : null}>다음</button>
       ) : null}
       {page > 0 ? (
         <button onClick={() => setPage(page - 1)}>이전</button>
       ) : null}
-      {page == 0 ? <button onClick={run}>다시 불러오기</button> : null
-      }
-      </>)
-  
+      {page == 0 ? <button onClick={run}>다시 불러오기</button> : null}
+    </>
+  );
 }
 
 export default Questions;
